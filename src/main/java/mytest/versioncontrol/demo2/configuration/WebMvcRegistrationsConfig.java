73874bf7bb47b11108df59a2e934edd9034cc9d1
wangheng <wangheng@foxmail.com>
2019-04-01 18:57:13 +0800

    增加注释

--- src/main/java/mytest/versioncontrol/demo2/configuration/WebMvcRegistrationsConfig.java
+++ src/main/java/mytest/versioncontrol/demo2/configuration/WebMvcRegistrationsConfig.java
@@ -4,8 +4,21 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+/**
+ * @Description
+ * @author wangheng
+ * @date 2019/4/1
+ * @return
+ */
 @SpringBootConfiguration
 public class WebMvcRegistrationsConfig implements WebMvcRegistrations {
+    /**
+     * @Description 重写请求过处理的方法
+     * @author wangheng
+     * @date 2019/4/1
+     * @param
+     * @return org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
+     */
     @Override
     public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
         return new CustomRequestMappingHandlerMapping();
